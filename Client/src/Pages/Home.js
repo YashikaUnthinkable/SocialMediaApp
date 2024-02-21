f3f6828cd86c090cda58e71dddbdb6d1606d2e3c
yashika <yashika.agarwal@unthinkable.com>
2024-02-21 12:48:02 +0530

    pagination with scroll

--- Client/src/Pages/Home.js
+++ Client/src/Pages/Home.js
@@ -1,100 +1,83 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useEffect, useState, useRef } from "react";
 import PostList from "../Components/PostList";
 import { useNavigate } from "react-router-dom";
+import debounce from "lodash/debounce";
 
 export default function Home(props) {
-  const nevigate = useNavigate();
+  const navigate = useNavigate();
   const [data, setData] = useState([]);
   const [totalPosts, setTotalPosts] = useState(0);
   const [postsCount, setPostsCount] = useState(0);
   const [count, setCount] = useState(0);
-  const [previous, setPrivious] = useState(true);
-  const [next, setNext] = useState(true);
+  const [fetchingData, setFetchingData] = useState(false); // Track if data is being fetched
+  const containerRef = useRef(null);
+
   useEffect(() => {
-    // for geting all posts data on initialization
+    fetchPosts();
+  }, [count]);
+
+  const fetchPosts = () => {
     try {
-      console.log("count:", count);
       if (count > postsCount) {
-        setNext(false);
+        return; // No more posts to fetch
       } else {
-        fetch("/api/posts" + "/" + count, {
-          method: "GET",
-        })
+        console.log(count);
+        fetch("/api/posts" + "/" + count)
           .then(async (response) => {
             const res_data = await response.json();
-            console.log(res_data);
-
-            setData(res_data.Posts);
+            setData((prevData) => [...prevData, ...res_data.Posts]);
+            console.log("Data: ", res_data.Posts);
             setTotalPosts(res_data.totalPosts);
             props.setUserId(res_data.id);
             setPostsCount(res_data.PostsCount);
-            if (count + 10 < res_data.PostsCount) {
-              setNext(true);
-              setPrivious(false);
-            }
+            console.log("called");
+            setFetchingData(false); // Reset flag when data fetching is complete
           })
           .catch((err) => {
             console.log(err);
+            setFetchingData(false); // Reset flag on error as well
           });
       }
     } catch (error) {
       console.log(error);
-    }
-  }, [, count]);
-
-  const setPaging = (e) => {
-    console.log("Data: ",data);
-    const n = e.target.name;
-    if (n === "privious") {
-      let newCount = count - 10;
-      setCount(newCount);
-      if (count == 0) {
-      }
-    } else {
-      let newCount = count + 10;
-      setCount(newCount);
-      if(count>postsCount){
-        setNext(false);
-      }
-      setPrivious(true);
-
-      setData([]);
+      setFetchingData(false); // Reset flag on error as well
     }
   };
-  const previousButton = (
-    <button className="btn btn-primary m-2" name="privious" onClick={setPaging}>
-      Privious
-    </button>
-  );
-  const nextButton = (
-    <button className="btn btn-primary m-2" name="next" onClick={setPaging}>
-      Next 
-    </button>
-  );
+
+  const handleScroll = debounce(() => {
+    const container = containerRef.current;
+    if (container && !fetchingData) {
+      const { scrollTop, scrollHeight, clientHeight } = container;
+      const distanceToBottom = scrollHeight - scrollTop - clientHeight;
+      if (distanceToBottom <= 100) {
+        console.log('fetching more data');
+        setFetchingData(true); // Set flag to indicate data fetching is in progress
+        setCount((prevCount) => prevCount + 10);
+      }
+    }
+  }, 500);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   if (props.isLoggedIn) {
-    console.log("Home: logged in");
     return (
       <div
-        className="d-flex flex-column-reverse align-items-center justify-content-center"
+        className="d-flex flex-column align-items-center justify-content-center"
         style={{ overflowY: "auto", marginTop: "70px" }}
+        ref={containerRef}
       >
-        <PostList data={data} setData={setData} userId={props.userId} />
         <div>
-          <h1>total no of posts are: {totalPosts}</h1>
+          <h1>Total number of posts: {postsCount}</h1>
         </div>
-        <div
-          className=" d-flex flex-row position-fixed fixed-bottom bg-white "
-          style={{ justifyContent: "flex-end" }}
-        >
-          {previous ? previousButton : ""}
-          {next ? nextButton : ""}
-        </div>
+        <PostList data={data} setData={setData} userId={props.userId} />
+        
       </div>
     );
   } else {
-    // if not login nevigate to the login page
-    nevigate("/login");
+    navigate("/login");
+    return null;
   }
 }
